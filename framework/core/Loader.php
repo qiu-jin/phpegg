a22fe6eb9f45e0ad23cc514a06ae8e460f1dcb35
qiujin <qiu-jin@qq.com>
2017-12-25 21:34:46 +0800

    update app mode

--- framework/core/Loader.php
+++ framework/core/Loader.php
@@ -71,7 +71,7 @@ class Loader
      */
     private static function autoload($class)
     {
-        if(($prefix = strstr($class, '\\', true)) && isset(self::$class_prefix[$prefix])) {
+        if(($prefix = strstr($class, '\\', true)) && isset(self::$class_prefix[$arr])) {
             self::import(self::$class_prefix[$prefix].substr(strstr(strtr($class, '\\', '/'), '/'), 1));
         } elseif(isset(self::$class_map[$class])) {
             self::import(self::$class_map[$class]);
